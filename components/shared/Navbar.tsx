c1c98e40799d19ca139c8a58566cf506cc40648a
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-08-22 23:13:44 +0800

    chore: navbar + footer

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,8 +1,10 @@
 import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthProviders from './AuthProviders'
 
 const Navbar = () => {
+    const session = {}
   return (
     <nav className='flexBetween navbar'>
         <div className='flex-1 flexStart gap-10'>
@@ -17,7 +19,7 @@ const Navbar = () => {
                 />
             </Link>
 
-            <ul className='xl: flex hidden text-small gap-7'>
+            <ul className='xl:flex hidden text-small gap-7'>
                 {NavLinks.map((link) => (
                     <Link
                         href={link.href}
@@ -27,6 +29,19 @@ const Navbar = () => {
                     </Link>
                 ))}
             </ul>
+        </div>
+
+        <div className='flexCenter gap-4'>
+            {session ? (
+                <>
+                  User Photo
+                  <Link href='/create-project'>
+                    Share Work
+                  </Link>
+                </> 
+            ) : (
+                <AuthProviders/>
+            )}
         </div>
     </nav>
   )
