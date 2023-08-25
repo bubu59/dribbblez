e7f6c290224ba0a98f1e7a6b8d95188dbf842a82
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-08-25 14:08:23 +0800

    chore: tryna fix the fetchAllProjects error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { ProjectInterface } from '@/common.types'
+import { fetchAllProjects } from '@/lib/actions'
 
-const Home = () => {
+type SearchParams = {
+  category?: string | null;
+  endcursor?: string | null;
+}
+
+type Props = {
+  searchParams: SearchParams
+}
+
+type ProjectsSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[]
+    pageInfo: {
+      hasPreviousPage: boolean,
+      hasNextPage: boolean,
+      startCursor: string,
+      endCursor: string
+    }
+  }
+}
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+
+  const data = await fetchAllProjects(category, endcursor) as ProjectsSearch
+  
   return (
     <section className='flex-start flex-col paddings mb-16'>
         <h1>Categories</h1>
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,5 @@
 import { ProjectForm } from '@/common.types'
-import { createProjectMutation, createUserMutation, getUserQuery } from '@/graphql'
+import { createProjectMutation, createUserMutation, getUserQuery, projectsQuery } from '@/graphql'
 import { GraphQLClient } from 'graphql-request'
 
 const isProduction = process.env.NODE_ENV === 'production'
@@ -81,3 +81,9 @@ export const createNewProject = async(form: ProjectForm, creatorId:string, token
         return makeGraphQLRequest(createProjectMutation, variables)
     }
 }
+
+export const fetchAllProjects = (category?: string | null, endcursor?: string | null) => {
+    client.setHeader("x-api-key", apiKey);
+  
+    return makeGraphQLRequest(projectsQuery, { category, endcursor });
+};
